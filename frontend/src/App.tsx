c8567e4362c6a364d041b52a9b602eea63d800df
eyahyal <yelmaoui@gmail.com>
2026-04-14 12:05:18 +0100

    int pr

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import Menubar from './components/Menubar'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <Menubar isLoggedIn={true}/> */}
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Menubar.tsx
@@ -0,0 +1,15 @@
+
+// function Menubar(props : boolean) : JSX.element {
+//     return(
+//         <>
+//             {props.isLoggedIn ? (
+
+//             )
+//              : (
+
+//              )}
+//         </>
+//     )
+// }
+
+// export default Menubar
